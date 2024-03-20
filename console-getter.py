21ec9f6c6f31b454d28a29f56f7a91dfa796784c
Pavel <99139074+R1senDev@users.noreply.github.com>
2024-03-21 00:42:21 +0300

    Fix + New features

--- console-getter.py
+++ console-getter.py
@@ -6,9 +6,15 @@ try:
     from lib.parser import get_symbol_price, get_available_markets
     from datetime import datetime
 
-    pair = input('pair=')
-    market = input('market=')
-    interval = int(input('interval='))
+    pair = 'A'
+    pairs = []
+
+    while True:
+        pair = input(f'Pair #{len(pairs) + 1} (leave empty if you done): ')
+        if pair: pairs.append(pair)
+        else: break
+    market = input('Market (all/binance/coinbase/blockchain.com): ')
+    interval = int(input('Update interval (seconds): '))
     MARKETS = get_available_markets()
 
     print()
@@ -17,17 +23,27 @@ try:
         while True:
             time = datetime.now()
             if market == 'all':
-                print(f'[{time.strftime("%H:%M:%S")}] {pair.upper()}:')
-                for m in MARKETS:
-                    try:
-                        print(f'\t{m}: {str(get_symbol_price(pair, m)).rstrip("0")}')
-                    except (ValueError, KeyError):
-                        print(f'\t{m} provided no data about {pair}')
+                print(f'[{time.strftime("%H:%M:%S")}]')
+                for p in pairs:
+                    print(f'\t{p.upper()}:')
+                    for m in MARKETS:
+                        try:
+                            print(f'\t\t{m}: {str(get_symbol_price(p, m)).rstrip("0")}')
+                        except (ValueError, KeyError):
+                            print(f'\t\t{m} provided no data about {p.upper()}')
+                        except ConnectionRefusedError:
+                            print(f'\t\tWhoa! Seems like {m} banned your IP for a while! Try decrease Interval in future.\n\t\t\t(Reason: ConnectionRefusedError)')
             else:
-                print(f'[{time.strftime("%H:%M:%S")}] {pair.upper()}: {get_symbol_price(pair, market)} ({market})')
+                print(f'[{time.strftime("%H:%M:%S")}]')
+                for p in pairs:
+                    try:
+                        print(f'\t{p.upper()}: {get_symbol_price(p, market)} ({market})')
+                    except (ValueError, KeyError):
+                        print(f'\t{m} provided no data about {p.upper()}')
             sleep(interval)
     except KeyboardInterrupt:
         print('Bye! :)')
+        sleep(3)
 
 except:
 
