c893cb1a1ecb8d7015ae7df57b79ea996c30d53a
Pavel <99139074+R1senDev@users.noreply.github.com>
2024-03-21 00:24:20 +0300

    HotFix

--- console-getter.py
+++ console-getter.py
@@ -1,17 +1,35 @@
-from time import sleep
-from lib.parser import get_symbol_price
-from datetime import date, datetime
-
-pair = input('pair=')
-market = input('market=')
-interval = int(input('interval='))
-
-print()
+from traceback import format_exc
 
 try:
-    while True:
-        time = datetime.now()
-        print(f'[{time.strftime("%H:%M:%S")}] {pair.upper()}: {get_symbol_price(pair, market)}')
-        sleep(interval)
-except KeyboardInterrupt:
-    print('Bye! :)')
+
+    from time import sleep
+    from lib.parser import get_symbol_price, get_available_markets
+    from datetime import datetime
+
+    pair = input('pair=')
+    market = input('market=')
+    interval = int(input('interval='))
+    MARKETS = get_available_markets()
+
+    print()
+
+    try:
+        while True:
+            time = datetime.now()
+            if market == 'all':
+                print(f'[{time.strftime("%H:%M:%S")}] {pair.upper()}:')
+                for m in MARKETS:
+                    try:
+                        print(f'\t{m}: {str(get_symbol_price(pair, m)).rstrip("0")}')
+                    except (ValueError, KeyError):
+                        print(f'\t{m} provided no data about {pair}')
+            else:
+                print(f'[{time.strftime("%H:%M:%S")}] {pair.upper()}: {get_symbol_price(pair, market)} ({market})')
+            sleep(interval)
+    except KeyboardInterrupt:
+        print('Bye! :)')
+
+except:
+
+    with open('traceback.txt', 'w') as file:
+        file.write(format_exc())
