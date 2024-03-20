e7e690d6aba6c3e7c23132b60068a9aa9b9e2991
Pavel <99139074+R1senDev@users.noreply.github.com>
2024-03-20 16:30:01 +0300

    first commit

---
+++ cryptoanalyzer.py
@@ -0,0 +1,68 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QWidget, QPushButton, QGridLayout, QCheckBox, QLineEdit
+from PyQt6.QtCore    import QAbstractTableModel, Qt
+from PyQt6.QtGui     import QIcon
+
+from requests.exceptions import ConnectionError, ConnectTimeout
+from ctypes              import windll
+
+from lib.parser import get_available_markets, get_symbol_price
+
+
+app = QApplication([])
+
+
+def open_symbol():
+    try:
+        price = get_symbol_price(pair_input.text() if pair_input.text().count('/') else f'{pair_input.text()}/{default_currency_select.currentText()}', market_select.currentText())
+        print(price)
+        windll.user32.MessageBoxW(
+	    	0,
+	    	(pair_input.text() if pair_input.text().count('/') else f'{pair_input.text()}/{default_currency_select.currentText()}') + f': {price}',
+	    	'CryptoAnalyzer',
+	    	0x00000040
+	    )
+    except (ConnectionError, ConnectTimeout):
+        windll.user32.MessageBoxW(
+	    	0,
+	    	'Не удалось выполнить запрос. Проверьте, подключены ли вы к сети.\n\nТекст исключения сброшен в файл \'traceback.txt\'',
+	    	'CryptoAnalyzer',
+	    	0x00000010
+	    )
+    except (KeyError, ValueError):
+        windll.user32.MessageBoxW(
+	    	0,
+	    	f'У {market_select.currentText()} нет данных о валютной паре ' + (pair_input.text() if pair_input.text().count('/') else f'{pair_input.text()}/{default_currency_select.currentText()}') + '.',
+	    	'CryptoAnalyzer',
+	    	0x00000030
+	    )
+
+
+window = QMainWindow()
+window.setWindowTitle('CryptoAnalyzer')
+window.setAcceptDrops(True)
+
+layout = QGridLayout()
+
+layout.addWidget(QLabel('Валютная пара'), 0, 0)
+layout.addWidget(pair_input := QLineEdit('BTC'), 0, 1)
+
+layout.addWidget(QLabel('Валюта по умолчанию'), 1, 0)
+layout.addWidget(default_currency_select := QComboBox(), 1, 1)
+default_currency_select.addItems(['USDT', 'USD', 'BTC', 'RUB'])
+
+layout.addWidget(QLabel('Источник информации'), 2, 0)
+layout.addWidget(market_select := QComboBox(), 2, 1)
+market_select.addItems(get_available_markets())
+
+layout.addWidget(open_symbol_btn := QPushButton('Открыть'), 3, 0, 1, 2)
+open_symbol_btn.clicked.connect(open_symbol)
+layout.addWidget(exit_btn := QPushButton('Выход'), 4, 0)
+layout.addWidget(open_settings_btn := QPushButton('Настройки'), 4, 1)
+
+wrapper = QWidget()
+wrapper.setLayout(layout)
+window.setCentralWidget(wrapper)
+window.show()
+
+
+app.exec()
---
+++ lib/parser.py
@@ -0,0 +1,52 @@
+from requests import get, Response
+
+MARKETS = {
+    'binance': {
+        'name':    'Binance',
+        'api_url': 'https://api.binance.com/api/v3/ticker/price?symbol={}'
+    },
+    'coinbase': {
+        'name':    'Coinbase',
+        'api_url': 'https://api.coinbase.com/v2/exchange-rates?currency={}'
+    },
+    'blockchain.com': {
+        'name':    'Blockchain.com',
+        'api_url': 'https://blockchain.info/tobtc?currency={}&value=1'
+    }
+}
+
+
+def get_available_markets() -> list[str]:
+    return sorted([MARKETS[market]['name'] for market in MARKETS])
+
+
+def get_symbol_price(pair: str, market: str = 'Binance', prefer_currency: str = 'USDT') -> float | None:
+    market = market.lower()
+    pair = pair.upper()
+    match market:
+        case 'binance':
+            return get(MARKETS[market]['api_url'].format(pair.replace('/', ''))).json()['price']
+        case 'coinbase':
+            return get(MARKETS[market]['api_url'].format(pair.split('/')[0])).json()['data']['rates'][pair.split('/')[1] if len(pair.split('/')) > 1 else prefer_currency.upper()]
+        case 'blockchain.com':
+            if (pair.split('/')[1] if len(pair.split('/')) > 1 else prefer_currency.upper()) != 'BTC':
+                raise ValueError('"blockchain.com" supports conversion to BTC only')
+            return get(MARKETS[market]['api_url'].format(pair.split('/')[0])).json()
+    raise ValueError(f'No such market: "{market}"')
+
+def get_symbols_prices(pairs: list[str], market: str = 'Binance') -> dict[str, float | None]:
+    out = {}
+
+    for pair in pairs:
+        data = get(MARKETS[market.lower()]['api_url'].format(pair.upper())).json()
+
+        if 'code' in data:
+            out[pair.upper()] = data['msg']
+        else:
+            out[data['symbol']] = data['price']
+    
+    return out
+
+if __name__ == '__main__':
+    from sys import argv
+    print(get_symbols_prices(argv[1:]))
